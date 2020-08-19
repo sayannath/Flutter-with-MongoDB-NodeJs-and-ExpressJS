bc28834dc9b7a9ab9cad974d0a8020fa8abda725
sayannath <sayannath235@gmail.com>
2020-08-19 23:18:44 +0530

    loginPage.dart

--- backend_app/lib/main.dart
+++ backend_app/lib/main.dart
@@ -1,65 +1,81 @@
+import 'package:backend_app/view/loginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Backend App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData.dark(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  SharedPreferences sharedPreferences;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    if (sharedPreferences.getString("token") == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+          (Route<dynamic> route) => false);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        appBar: AppBar(
+          title: Text('Product'),
+          centerTitle: true,
+          actions: [
+            FlatButton(
+              onPressed: () {
+                sharedPreferences.clear();
+                sharedPreferences.commit();
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => LoginPage()),
+                    (Route<dynamic> route) => false);
+              },
+              child: Icon(Icons.power_settings_new),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        body: Container(
+          child: Text('Main Body'),
+        ),
+        drawer: Drawer(
+          child: ListView(children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text('NodeJs'), accountEmail: Text('Email')),
+            ListTile(
+              title: Text('List Products'),
+              trailing: Icon(Icons.list),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Add Products'),
+              trailing: Icon(Icons.add),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Register user'),
+              trailing: Icon(Icons.add),
+              onTap: () {},
+            )
+          ]),
+        ));
   }
 }
